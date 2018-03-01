269069ddd5241c9ae29dfc31f68e195b92d5e98c
Pedro Henrique Pereira Lima <phenriquelima.ph@gmail.com>
2018-02-28 22:50:31 -0300

    Add webpack and devise.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
 	acts_as_list
 
+	scope :sorted, -> { order(position: :asc) }
+
 	has_many :cards, -> { order(position: :asc) }, dependent: :destroy
 
 	validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	namespace :admin do
+		resources :users
+		resources :announcements
+		resources :notifications
+
+		root to: 'users#index'
+	end
+
+	get '/privacy', to: 'home#privacy'
+	get 'terms', to: 'home#terms'
+
+	resources :notifications, only: [:index]
+	resources :announcements, only: [:index]
+
+	authenticate :user, ->(u) { u.admin? } do
+		mount Sidekiq::Web => '/sidekiq'
+	end
+
+	devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+	resources :lists
+	resources :cards
+
+	root to: 'lists#index'
 end
